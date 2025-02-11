0f251f9b43271e5954328a5ca4fbc8d2bbd75f5a
SeanSchoolfield <sean.schoolfield@gmail.com>
2025-02-11 13:16:37 -0600

    Added a dragbox feature. Can now select drawn features and delete them.

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ import TileLayer from 'ol/layer/Tile.js';
 import VectorLayer from 'ol/layer/Vector.js';
 import OSM from 'ol/source/OSM';
 import VectorSource from 'ol/source/Vector.js';
+import DragBox from 'ol/interaction/DragBox.js';
+import Select from 'ol/interaction/Select.js';
+import { platformModifierKeyOnly } from 'ol/events/condition.js';
 
 const raster = new TileLayer({
     source: new OSM(),
@@ -47,3 +50,48 @@ typeSelect.onchange = function () {
 document.getElementById('undo').addEventListener('click', function () { draw.removeLastPoint(); });
 
 addInteraction();
+
+//DragBox implementation
+
+const dragBox = new DragBox({
+    condition: platformModifierKeyOnly, //CTRL key
+});
+
+let selectedFeatures = new Select();
+map.addInteraction(selectedFeatures);
+
+dragBox.on('boxend', function () {
+    const extent = dragBox.getGeometry().getExtent();
+
+    selectedFeatures.getFeatures().clear();
+
+    source.forEachFeatureIntersectingExtent(extent, function (feature) {
+        selectedFeatures.getFeatures().push(feature);
+    });
+
+    const numSelected = selectedFeatures.getFeatures().getLength();
+    if (numSelected > 0) {
+        alert('${numSelected} feature(s) selected.');                               //swap out alert for something else that doesn't require confirmation
+        deleteButton.disabled = false;
+    } else {
+        alert("No features selected.");                                             //swap out alert for something else that doesn't require confirmation
+        deleteButton.disabled = true;
+    }
+});
+
+document.getElementById('select-mode').addEventListener('click', function () {
+    if (!map.getInteractions().getArray().includes(dragBox)) {
+        map.addInteraction(dragBox);
+        alert("Hold CTRL and drag to select features.");                            //swap out alert for something else that doesn't require confirmation
+    }
+});
+
+const deleteButton = document.getElementById('delete-selected');
+deleteButton.addEventListener('click', function () {
+    selectedFeatures.getFeatures().forEach(function (feature) {
+        source.removeFeature(feature);
+    });
+    selectedFeatures.getFeatures().clear();
+    deleteButton.disabled = true;
+    alert("Selected features deleted.");                                            // modify this to allow for a cancel
+});
